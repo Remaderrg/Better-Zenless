19457d3bd28c4abf1e013f8af54db5d301d14551
Remader <remadergames@gmail.com>
2023-02-24 18:13:26 +0300

    0.4

--- my-style.js
+++ my-style.js
@@ -3,9 +3,10 @@
     var head = document.getElementsByTagName('head')[0];
     var style = document.createElement('style');
     head.appendChild(style);
-    style.innerHTML = '.loading[data-v-7df5392c] { visibility: hidden; } .loading[data-v-0be5b554] { visibility: hidden; } * { user-select: text; } .footer__socialbar[data-v-669308e5], .footer__socialbar[data-v-7ecc1c92] {justify-content: center; border-bottom: 0px;} ::selection {background: #333; color: #fff;/* Safari */} ::-moz-selection {background: #333; color: #fff;/* Firefox */}';
+    style.innerHTML = '.loading[data-v-4797d62b] { visibility: hidden; } .loading[data-v-0be5b554] { visibility: hidden; } * { user-select: text; } .footer__socialbar[data-v-669308e5], .footer__socialbar[data-v-7ecc1c92] {justify-content: center; border-bottom: 0px;} ::selection {background: #333; color: #fff;/* Safari */} ::-moz-selection {background: #333; color: #fff;/* Firefox */}';
     localStorage.setItem('napSeaBgAudioOfficial', '{"data":false}');
     localStorage.setItem('@me/audio_cache_by_zzz.mihoyo/', '{"data":false}');
+    localStorage.setItem('napCnBgAudioOfficial', '{"data":false}');
   });
 })(this, this.document);
 
